7fa9a3c63fabf8a0e18f7a033cb7a44ba2db8c18
iprimavera <elsprimavera@gmail.com>
2024-10-24 11:52:53 +0200

    finished file mochila.sh

--- bucles/arkham/mochila.sh
+++ bucles/arkham/mochila.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
-
-#lista de numeros sin repetir entre el 10 100, 40 num
-#dado un numero de cosas por el usuario decir cuales son las x cosas de mayor valor
+echo ""
 
 lista=()
 
-for i in $(seq 40); do
-	r=$((RANDOM%100+1))
-	echo "$r"
+#lista de numeros sin repetir entre el 10 100, 40 num
+
+while [ ${#lista[@]} -lt 40 ]; do
+
+	r=$((RANDOM%90 + 10))
+
+	for i in ${lista[@]}; do
+		if [[ $i == $r ]]; then
+#			r=$((RANDOM%100+1))
+			continue 2
+		fi
+	done
+
+	lista+=($r)
+
 done
 
 echo ${lista[@]}
 
+#dado un numero de cosas por el usuario decir cuales son las x cosas de mayor valor
+
+read -p "Cuantas cosas quieres llevarte?  " ncosas
+
+cosas=0
+newlista=()
+
+for i in $(seq 100 -1 10); do
+	if [ $cosas -lt $ncosas ]; then
+		for a in ${lista[@]}; do
+			if [ $i -eq $a ]; then
+				((cosas ++))
+				newlista+=($a)
+			fi
+		done
+	fi
+done
+
+echo "las cosas que te puedes llevar son: ${newlista[@]}"
+
+echo ""
 exit 0
