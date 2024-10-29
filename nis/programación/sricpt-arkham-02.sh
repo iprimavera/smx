ce1c24749ffd786f5bec787ba77ab7b8951ee0ba
nissk8 <tec1esod10@gmail.com>
2024-10-29 12:02:00 +0100

    archivo

---
+++ nis/programación/sricpt-arkham-02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p $HOME/Arkham/Hangares/Terrar
+mkdir -p $HOME/Arkham/Hangares/Profur
+mkdir -p $HOME/Arkham/Hangares/Medea
+
+####
+## FRAGMENTO PERDIDO
+###
+
+
+ls -l $HOME/Arkham/Hangares/Habilitado
+
+exit 0
