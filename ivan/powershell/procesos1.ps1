a2572426081e00119eed72589f4a7ef2dd68eb79
Ivan <elsprimavera55@gmail.com>
2024-11-21 19:40:02 +0100

    added kirchoff.ps1

---
+++ ivan/powershell/procesos1.ps1
@@ -0,0 +1,14 @@
+if ($($args.Count) -eq 1) {
+    if ($args[0] -ge 1 -and $args[0] -le 10000) {
+        try {
+            Get-Process -Id $args[0] -ErrorAction Stop | Select-Object ProcessName
+        }
+        catch {
+            Write-Host "No hay ningun proceso activo con ese id" -ForegroundColor Red
+        }
+    } else {
+        Write-Host "El numero debe estar entre el 1 y el 10000" -ForegroundColor Red
+    }
+} else {
+    Write-Host "Solo se debe pasar un unico argumento" -ForegroundColor Red
+}
---
+++ ivan/powershell/procesos2.ps1
@@ -0,0 +1,40 @@
+while ($true) {
+    Write-Host "Selecciona una opcion:"
+    Write-Host "1) Uso de la CPU de un proceso"
+    Write-Host "2) PATH del ejecutable de un proceso"
+    Write-Host "3) exit"
+
+    # echo de menos el select de bash :(
+
+    $opcion = Read-Host
+
+    switch ($opcion) {
+        1 {
+            $proceso = Read-Host "Ingresa el PID"
+            try {
+                Get-Process -Id $proceso -ErrorAction Stop | Select-Object ProcessName, CPU
+            }
+            catch {
+                Write-Host "No hay ningun proceso activo con ese id" -ForegroundColor Red
+            }
+        }
+        2 {
+            $proceso = Read-Host "Ingresa el PID"
+            try {
+                Get-Process -Id $proceso -ErrorAction Stop | Select-Object ProcessName, Path
+            }
+            catch {
+                Write-Host "No hay ningun proceso activo con ese id" -ForegroundColor Red
+            }
+        }
+        3 {
+            exit
+        }
+        "exit" {
+            exit
+        }
+        default {
+            Write-Host "Opcion no valida" -ForegroundColor Red
+        }
+    }
+}
