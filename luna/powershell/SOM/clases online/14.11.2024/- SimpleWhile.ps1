7da230b13d84e4f4cc0f5ae2fa96f3a559af1180
Lunaval156 <lunavalero5@gmail.com>
2024-11-27 22:53:30 +0100

    fgf

---
+++ luna/powershell/SOM/clases online/14.11.2024/- SimpleWhile.ps1
@@ -0,0 +1,16 @@
+﻿#Script que pregunte al usuario por un número
+#mientras que el total acumulado sea menor que 20
+#que vuleva a preguntar, cuando lo superemos 
+#que muestre un mensaje.
+
+$CONTADOR = 0
+
+while ( $CONTADOR -lt 20 ){
+
+    Write-Host "Dime un número:"
+    $NUM = Read-Host
+    $CONTADOR = $CONTADOR + $NUM
+
+}
+
+Write-Host "Hemos llegado a $CONTADOR que es mayor o igual que 20"
---
+++ luna/powershell/SOM/clases online/24.11.24/Definicion.ps1
@@ -0,0 +1,4 @@
+Get-ChildItem -recurse | measure -sum Length
+
+Este comando calcula y muestra el tamaño total en bytes de todos los archivos dentro del 
+directorio especificado y sus subdirectorios.
