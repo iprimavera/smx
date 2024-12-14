8eb882ee158493617f70c91ee7a7ceecb20d5af0
Ivan <elsprimavera55@gmail.com>
2024-12-14 13:27:51 +0100

    added task 1 software

--- ivan/powershell/software/1/tarea2.ps1
+++ ivan/powershell/software/1/tarea2.ps1
@@ -5,3 +5,35 @@
 #En caso de que no haya ningún software anterior, que muestre un mensaje
 #indicándolo. Si un software no tiene ninguna fecha de instalación, asumir
 #que es del tres de enero de 2021.
+
+if ($args.Count -eq 1)
+{
+    $numero = 0
+
+    $argumento = $([int]$args[0])
+
+    Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | select-object DisplayName, InstallDate | ForEach-Object {
+        $hola = $_.InstallDate
+        $hola = $([int]$hola)
+
+        if ($hola -eq 0)
+        {
+            $hola = 20210103
+        }
+        
+        if ($hola -lt $argumento)
+        {
+            Write-Host $_.DisplayName $hola
+            $numero = $numero + 1
+        }
+    }
+
+    if ($numero -eq 0)
+    {
+        Write-Host "No hay ningun software instalado antes de la fecha $argumento" -ForegroundColor Red
+    }
+}
+else
+{
+    Write-Host "Numero incorrecto de argumentos" -ForegroundColor Red
+}
